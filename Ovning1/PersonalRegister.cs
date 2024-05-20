14d6b33b64cc8a04a1aff52d98ff80ba22d49517
Karsten Tvarnö <vvarrior@gmail.com>
2024-05-20 15:03:02 +0200

    Add project files.

---
+++ Ovning1/PersonalRegister.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovning1
+{
+    internal class PersonalRegister
+    {
+        private string name;
+        private double salery;
+
+        public string Name
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        public double Salery
+        { 
+            get { return salery; } 
+            set {  salery = value; } 
+        }
+    }
+}
---
+++ Ovning1/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Ovning1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PersonalRegister> personalList = new List<PersonalRegister>();
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+                Console.WriteLine("Options: ");
+                Console.WriteLine("Option 1. List employees and salary.");
+                Console.WriteLine("Options 2. Enter new employee and salary.");
+                Console.WriteLine("Option 3. Terminate Program.");
+                Console.Write("Enter choise: ");
+
+                string option = Console.ReadLine();
+
+                if (option == "1")
+                    ListEmployees(personalList);
+                else if (option == "2")
+                    AddEmployee();
+                else if (option == "3")
+                    running = false;
+                
+            }
+
+            void AddEmployee()
+            {
+                PersonalRegister personalRegister = new PersonalRegister();
+
+                Console.Write("Enter Name: ");
+                personalRegister.Name = Console.ReadLine();
+                Console.Write("Enter Salary: ");
+                personalRegister.Salery = Convert.ToDouble(Console.ReadLine());
+
+                personalList.Add(personalRegister);
+            }
+
+            void ListEmployees(List<PersonalRegister> personal)
+            {
+                foreach (var item in personal)
+                    Console.WriteLine($"Employee name {item.Name} and current salary is {item.Salery}");
+
+                Console.WriteLine("Press Key To Continue:");
+                Console.ReadLine();
+            }
+        }
+    }
+}
